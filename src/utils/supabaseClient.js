de7d758b65cc653415ac8e2ddda1e44a6eedaca6
youssef osama <youssefosama101110@gmail.com>
2026-06-02 09:49:02 +0300

    debug env

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient( supabaseUrl, supabaseKey);
+console.log("SUPABASE_URL =", supabaseUrl);
+console.log("SUPABASE_KEY =", supabaseKey);
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
